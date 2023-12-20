3ed4057d7f1b23e2b6922404eac7a71afa486fd3
Leman300 <leman300@wp.pl>
2023-12-20 21:35:05 +0100

    Added new base direction

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/first-react-website/",
+  base: "/INSIGHT/",
   plugins: [react()],
   test: {
     globals: true,
