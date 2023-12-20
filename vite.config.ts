e5f7130868c4bb2ac995bbcb43c0a2e943aab83e
Leman300 <leman300@wp.pl>
2023-12-20 21:32:42 +0100

    Change file name and add direction of config base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/first-react-website/",
   plugins: [react()],
   test: {
     globals: true,
